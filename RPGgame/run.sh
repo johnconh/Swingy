d70030643851910f0d37803809af4b75570b7143
johnconh <sevenballs@gmail.com>
2024-10-21 19:45:57 +0200

    mejorado

--- RPGgame/run.sh
+++ RPGgame/run.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 
-# Limpiar el proyecto
-mvn clean
-
-# Compilar el proyecto y empaquetarlo en un .jar
-mvn package
-
-# Ejecutar el archivo .jar (ajusta la ruta del archivo .jar si es necesario)
+mvn clean package
 java -jar target/RPGgame-1.0.jar
